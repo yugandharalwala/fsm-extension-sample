0196b008170b1ae02e53c71f0c2873314f9e7435
yugandharalwala402 <yyugandhargoud@deloitte.com>
2024-01-16 15:21:25 +0530

    testing

--- samples/service-contract/helpers.js
+++ samples/service-contract/helpers.js
@@ -38,43 +38,61 @@ function getServiceContract(cloudHost, account, company, activity_id) {
     'X-Client-Version': '1.0.0',
     'Authorization': `bearer ${sessionStorage.getItem('token')}`,
   };
-
+  const tagBody={
+      "filter": [{
+        "field": "name",
+        "operator": "=",
+        "value": "0020-006"
+      }], 
+      "page": 0,
+      "size": 20 
+    }
+  
   return new Promise(resolve => {
-
-    // Fetch Activity object
-    fetch(`https://${cloudHost}/api/data/v4/Activity/${activity_id}?dtos=Activity.37&account=${account}&company=${company}`, {
+    fetch(`https://${cloudHost}/cloud-skill-service/api/v1/tags/search?account=${account}&company=${company}`, {
+      method: "POST",
+      tagBody,
       headers
-      })
-        .then(response => response.json())
-        .then(function(json) {
+      }).then(response => response.json()).then(function(json) {updateUI(json)});
+  }
 
-          const activity = json.data[0].activity;
-          // Fetch all ServiceContractEquipment
-          fetch(`https://${cloudHost}/api/data/v4/ServiceContractEquipment?dtos=ServiceContractEquipment.12&account=${account}&company=${company}`, {
-            headers
-            })
-              .then(response => response.json())
-              .then(function(json) {
 
-                const serviceContractEquipment = json.data.find(contract => contract.serviceContractEquipment.equipment === activity.equipment);
-                if (!serviceContractEquipment) {
-			        console.log('Yugandhar Testing 1');
-                  resolve(null);
-                } else {
-                  fetch(`https://${cloudHost}/api/data/v4/ServiceContract/${serviceContractEquipment.serviceContractEquipment.serviceContract}?dtos=ServiceContract.13&account=${account}&company=${company}`, {
-                    headers
-                    })
-                      .then(response => response.json())
-                      .then(function(json) {
-			                console.log('Yugandhar Testing 2');
-                        resolve(json.data[0].serviceContract);
-                      });
-                }
+  // return new Promise(resolve => {
 
-              });
+  //   // Fetch Activity object
+  //   fetch(`https://${cloudHost}/api/data/v4/Activity/${activity_id}?dtos=Activity.37&account=${account}&company=${company}`, {
+  //     headers
+  //     })
+  //       .then(response => response.json())
+  //       .then(function(json) {
 
-        });
+  //         const activity = json.data[0].activity;
+  //         // Fetch all ServiceContractEquipment
+  //         fetch(`https://${cloudHost}/api/data/v4/ServiceContractEquipment?dtos=ServiceContractEquipment.12&account=${account}&company=${company}`, {
+  //           headers
+  //           })
+  //             .then(response => response.json())
+  //             .then(function(json) {
 
-  });
+  //               const serviceContractEquipment = json.data.find(contract => contract.serviceContractEquipment.equipment === activity.equipment);
+  //               if (!serviceContractEquipment) {
+	// 		        console.log('Yugandhar Testing 1');
+  //                 resolve(null);
+  //               } else {
+  //                 fetch(`https://${cloudHost}/api/data/v4/ServiceContract/${serviceContractEquipment.serviceContractEquipment.serviceContract}?dtos=ServiceContract.13&account=${account}&company=${company}`, {
+  //                   headers
+  //                   })
+  //                     .then(response => response.json())
+  //                     .then(function(json) {
+	// 		                console.log('Yugandhar Testing 2');
+  //                       resolve(json.data[0].serviceContract);
+  //                     });
+  //               }
+
+  //             });
+
+  //       });
+
+  // });
 }
 
