5609458b0c639134bf2c18c1b8a915556acea828
yugandharalwala402 <yyugandhargoud@deloitte.com>
2025-01-26 20:22:52 +0530

    updating the code

--- samples/service-contract/helpers.js
+++ samples/service-contract/helpers.js
@@ -1,167 +1,75 @@
-//
+
+// 
 // Update html dom with provided string value
 //
 const updateUI = (text) =>
-   (document.querySelectorAll('#info')[0].innerText = text);
+  (document.querySelectorAll('#info')[0].innerText = text);
 
 //
 // Loop before a token expire to fetch a new one
 //
 function initializeRefreshTokenStrategy(shellSdk, auth) {
 
-   shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
-      sessionStorage.setItem('token', event.access_token);
-      setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
-   });
+  shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
+    sessionStorage.setItem('token', event.access_token);
+    setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
+  });
 
-   function fetchToken() {
-      shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
-         response_type: 'token' // request a user token within the context
-      });
-   }
+  function fetchToken() {
+    shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
+      response_type: 'token'  // request a user token within the context
+    });
+  }
 
-   sessionStorage.setItem('token', auth.access_token);
-   setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
+  sessionStorage.setItem('token', auth.access_token);
+  setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
 }
 
-//
+// 
 // Request context with activity ID to return serviceContract assigned
 //
-function getServiceContract(cloudHost, account,accountId, company,companyId, activity_id) {
-   document.getElementById('test').innerHTML='';
-   const headers = {
-      'Content-Type': 'application/json',
-      'X-Client-ID': 'fsm-extension-sample',
-      'X-Client-Version': '1.0.0',
-      'Authorization': `bearer ${sessionStorage.getItem('token')}`
-   };
+function getServiceContract(cloudHost, account, company, activity_id) {
+  
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-Client-ID': 'fsm-extension-sample',
+    'X-Client-Version': '1.0.0',
+    'Authorization': `bearer ${sessionStorage.getItem('token')}`,
+  };
 
-   const personsUids = new Map();
-   var tagIds = [];
+  return new Promise(resolve => {
 
-   var t0Credentials="grant_type=client_credentials&client_id=000178da-38e4-471a-bfa0-0e7fb67bf90b&client_secret=e217dcc4-1107-41f5-a0a6-1b34a7652a16";
-   var t1Credentials="grant_type=client_credentials&client_id=000176ec-eb15-4c2a-b9c7-d3e28ddfd0a1&client_secret=30b22792-0cf4-49a1-91c1-d202336f5378";
+    // Fetch Activity object
+    fetch(`https://${cloudHost}/api/data/v4/Activity/${activity_id}?dtos=Activity.37&account=${account}&company=${company}`, {
+      headers
+      })
+        .then(response => response.json())
+        .then(function(json) {
 
+          const activity = json.data[0].activity;
+          // Fetch all ServiceContractEquipment
+          fetch(`https://${cloudHost}/api/data/v4/ServiceContractEquipment?dtos=ServiceContractEquipment.12&account=${account}&company=${company}`, {
+            headers
+            })
+              .then(response => response.json())
+              .then(function(json) {
 
-   return new Promise(resolve => {
-      fetch(`https://auth.coresuite.com/api/oauth2/v1/token`, {
-         method: "POST",
-         body: accountId=='95980'?`${t1Credentials}`:`${t0Credentials}`,
-         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-         }
-      }).then(response => response.json()).then(function (json) {
-         const personQuery = `SELECT DISTINCT u.id,u.firstName+' '+u.lastName as name FROM UnifiedPerson u JOIN Region r ON r.id IN u.regions JOIN Activity a ON r.externalId=a.udf.zActWLA WHERE a.id='${activity_id}'`;
-         const tagsQuery = `SELECT r.tag from Requirement r WHERE r.object.objectId='${activity_id}' AND r.mandatory=TRUE`;
-         //fetch persons based on activity region
-         fetch(`https://${cloudHost}/api/query/v1?account=${account}&company=${company}&dtos=UnifiedPerson.13;Region.10;Activity.13`, {
-            method: "POST",
-            body: JSON.stringify({
-               "query": personQuery
-            }),
-            headers: {
-               'Content-Type': 'application/json',
-               'X-Client-ID': 'fsm-extension-sample',
-               'X-Client-Version': '1.0.0',
-               'Authorization': `bearer ${json.access_token}`
-            }
-         }).then(response1 => response1.json()).then(function (json1) {
-            //updateUI(JSON.stringify(json1))
-            json1.data.forEach(function (currentValue) {
-               personsUids.set(currentValue.u.id, currentValue.name)
-            });
-            console.log(personsUids);
-         }).then(function (json2) {
-            //fetch tag ids by passing activity ids to requirement table
-            fetch(`https://${cloudHost}/api/query/v1?account=${account}&company=${company}&dtos=Requirement.10`, {
-                  method: "POST",
-                  body: JSON.stringify({
-                     "query": tagsQuery
-                  }),
-                  headers: {
-                     'Content-Type': 'application/json',
-                     'X-Client-ID': 'fsm-extension-sample',
-                     'X-Client-Version': '1.0.0',
-                   'Authorization': `bearer ${json.access_token}`
-                    
-                  }
-               }).then(response2 => response2.json()).then(function (tags) {
+                const serviceContractEquipment = json.data.find(contract => contract.serviceContractEquipment.equipment === activity.equipment);
+                if (!serviceContractEquipment) {
+                  resolve(null);
+                } else {
+                  fetch(`https://${cloudHost}/api/data/v4/ServiceContract/${serviceContractEquipment.serviceContractEquipment.serviceContract}?dtos=ServiceContract.13&account=${account}&company=${company}`, {
+                    headers
+                    })
+                      .then(response => response.json())
+                      .then(function(json) {
+                        resolve(json.data[0].serviceContract);
+                      });
+                }
 
-                  tags.data.forEach(function (value) {
-                     tagIds.push(value.r.tag)
-                  });
-                  console.log(tagIds);
+              });
 
-               })
-               .then(function (json3) {
-                  json3=new Map();
-                  var skill=new Map();
-                  tagIds.forEach((tagId) => {
-                     json3.set(tagId,[]);
-                     personsUids.forEach((value, key) => {
-                        const tagTechbody = {
-                           "filter": [{
-                              "field": "technicianId",
-                              "operator": "=",
-                              "value": key
-                           }],
-                           "page": 0,
-                           "size": 20
-                        }
-                        fetch(`https://us.coresystems.net/cloud-skill-service/api/v1/tags/${tagId}/skills/search`, {
-                           method: "POST",
-                           body: JSON.stringify(tagTechbody),
-                           headers: {
-                              'Content-Type': 'application/json',
-                              'X-Client-ID': '000176ec-eb15-4c2a-b9c7-d3e28ddfd0a1',
-                              'X-Client-Version': 'v4',
-                              'X-Account-Id': `${accountId}`,
-                              'X-Account-Name': `${account}`,
-                              'X-Company-Id': `${companyId}`,
-                              'X-Company-Name': `${company}`,
-                              'Authorization': `bearer ${json.access_token}`
-                             
-                           }
-                        }).then(profResoonse => profResoonse.json()).then(function (profRes) {
-                           profRes.content.forEach(function (prof) {
-                              json3.get(tagId).push(`<div>Person:  ${personsUids.get(prof.technicianId)}</div><div class='under-line'>Proficiency level:  ${prof.proficiencyLevel}</div>`);
-                              skill.set(tagId,`<div>Skill Name: ${prof.tagName}</div>`);
-                              //var prevhtml = document.getElementById('test').innerHTML;
-                              //var htmlCode = prevhtml + `<div>Person:  ${personsUids.get(prof.technicianId)}</div><div class='under-line'>Proficiency level:  ${prof.proficiencyLevel}</div>`
-                              //console.log(`\n ${personsUids.get(prof.technicianId)}` + `\n Skill- ${prof.tagName} \n Skill proficiency level :${prof.proficiencyLevel}`);
-                              //document.getElementById('test').innerHTML = htmlCode;
-                             // document.getElementById('info').innerHTML = htmlCode;
-                                  
-                           });
+        });
 
-                        }).then(function(test){
-
-                           let htmlString='';
-                           json3.forEach(function(value,key){
-                              htmlString += skill.get(key)+value.join('');
-                           });
-                           if(htmlString!=''){
-                           document.getElementById('info').innerHTML =htmlString;
-                           }
-                           else{
-                              document.getElementById('info').innerHTML ='No Data Available';
-                           }
-                           console.log(json3);
-                           console.log(skill);
-                        });
-
-
-                     });
-
-
-                  });
-                  //
-                  
-
-               });
-
-         });
-      });
-
-   });
+  });
 }
